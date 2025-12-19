a49424ca5420760e35906fd8cddebac480881dc7
KanFoolyCooly <kantawitcoolkansorpromrach@gmail.com>
2025-12-19 15:48:03 +0700

    สร้างโดยใช้ Colab

---
+++ Mid-24-Kan.py
@@ -0,0 +1,70 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNYqvhM/NVq1JH9qaxAs2RW",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/KanFoolyCooly/24-Kan-Coding/blob/main/Mid-24-Kan.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 27,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "KudGQ7SRqz6d",
+        "outputId": "c25c7b1e-d9d0-4681-8eb8-1926cfa734c3"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "ใส่เลขครับหนู:  0\n",
+            "Zero\n"
+          ]
+        }
+      ],
+      "source": [
+        "number_str = input(\"ใส่เลขครับ:  \")\n",
+        "number = float(number_str)\n",
+        "if number > 0:\n",
+        "  print(\"Positive\")\n",
+        "elif number < 0:\n",
+        "    print(\"Negative\")\n",
+        "else:\n",
+        "    print(\"Zero\")"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "nJcZgLJNs43b"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
